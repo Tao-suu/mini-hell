40f3c2315dfaaeaede01860579ed1452313155e5
Pierre Chevaldonne <picheval@student.42.fr>
2026-02-01 05:56:37 +0100

    Expand refacto start

--- srcs/exec/expand.c
+++ srcs/exec/expand.c
@@ -6,7 +6,7 @@
 /*   By: picheval <picheval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:36:38 by tbez--du          #+#    #+#             */
-/*   Updated: 2026/02/03 01:34:09 by tbez--du         ###   ########.fr       */
+/*   Updated: 2026/02/03 01:37:12 by tbez--du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,8 @@ static int	expand_cmd(t_data *data, t_cmd *cmd)
 	}
 	return (TRUE);
 }
+
+// TODO
 
 int	expand_pipe(t_data *data, t_cmd *cmds)
 {
